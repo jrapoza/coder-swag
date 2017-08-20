60160bbe7919185ffeaeaaa5c15dc2ac42ead18a
Lynn Rapoza <jrapoza@Joe-Rapozas-Mac-Pro-2.local>
2017-08-20 12:10:03 -0700

    initial commit

---
+++ coder-swag-test/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  coder-swag-test
+//
+//  Created by Lynn Rapoza on 8/20/17.
+//  Copyright © 2017 Etherworld. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tableViewArea: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
